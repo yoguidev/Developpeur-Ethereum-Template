80b5b8007db464809de1be63a30483d22ea44c72
Lierville G <com@veryweb.fr>
2022-06-14 16:51:26 +0200

    v. 0.2

--- 3. Solidity/Voting.sol
+++ 3. Solidity/Voting.sol
@@ -2,7 +2,13 @@
 // Exercice de creation d'un system de vote
 // */
 
+// Désolé j'étais de mariage ce week end, il me manque donc une dizaine d'heures de travail ! :(
+
 pragma solidity 0.8.14;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+
+
 
 
 contract Voting {
@@ -14,10 +20,7 @@ contract Voting {
         bool hasVoted;
         uint votedProposalId;
     }
-    struct Proposal {
-        string description;
-        uint voteCount;
-    }
+    
 
     enum WorkflowStatus {
         RegisteringVoters,
@@ -33,10 +36,65 @@ contract Voting {
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
 
-// Creation de la liste blanche
 
-    contract Whitelist {
-        mapping(address=> bool) whitelist;
+
+// Début Creation de la liste blanche des votants
+
+    // Adresse ETH du propriétaire du contrat
+    address owner; 
+
+    constructor() {
+        owner = msg.sender; // On initialise l'adresse du propriétaire du contrat
     }
 
+    // Seul le proprietaire du contrat peut ajouter des adresses d'electeurs dans la liste blanche
+    modifier onlyOwner() {
+    require(msg.sender == owner, "Vous n'etes pas autorise a ajouter des votants");
+    _;
+    }
+
+    mapping(address => bool) whitelistedAddresses;
+
+    function addUser(address _addressToWhitelist) public onlyOwner {
+    whitelistedAddresses[_addressToWhitelist] = true;
+    }   
+
+    function verifyUser(address _whitelistedAddress) public view returns(bool) {
+    bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
+    return userIsWhitelisted;
+    }
+
+// Fin de la creation liste blanche des votants
+
+
+
+
+// L administrateur commence la session d enregistrement des propositions
+
+    struct Proposition {
+        string description;
+        uint voteCount;
+    }
+
+    mapping(address => string) public propositions;
+
+    // Fonction d'ajout des propositions
+    function newPropaOwner(string memory propa) public onlyOwner {
+
+        propositions[msg.sender] = propa;
+    }
+
+   
+// Les électeurs inscrits sont autorisés à enregistrer leurs propositions
+
+function newPropaAll(string memory propa) public {
+
+        propositions[msg.sender] = propa;
+    }
+
+
 }
+
+
+// Désolé j'étais de mariage ce week end, il me manque donc une dizaine d'heures de travail ! :(
+// J'ai donc fait ce que j'ai pu ...
